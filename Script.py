f6e38f4a55143c754fdb0a960fcabe0d91290f70
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-03-31 11:03:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,6 +18,11 @@ class script(object):
 <b>DEVS:</b>
 - 𝙳𝚎𝚟 1<a href=https://t.me/mr_MKN>𝙼𝚛.𝙼𝙺𝙽 𝚃𝙶</a>
 - 𝙳𝚎𝚟 2<a href=https://t.me/AboutAadhi>𝙰𝙰𝙳𝙷𝙸</a>"""
+    
+    CUSTOM_FILE_CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}</b>
+╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═════╗
+♻️ 𝙂𝙍𝙊𝙐𝙋:-https://t.me/+ebT3TGDywLM0OGY9        
+╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═════╝>"""
 
     FILE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞 𝐌𝐨𝐝𝐮𝐥𝐞../
 
